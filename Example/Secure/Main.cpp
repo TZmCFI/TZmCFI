7c8b48bac761952f7f6f543a9ef26908b5aeb450
Tomoaki Kawada <tcpp.jp@gmail.com>
2018-11-27 20:02:59 +0900

    Fix the boot loader for the latest version of QEMU

--- Example/Secure/Main.cpp
+++ Example/Secure/Main.cpp
@@ -24,32 +24,67 @@ typedef void (*ns_funcptr_void)(void) __attribute__((cmse_nonsecure_call));
     SAU->RBAR = 0x00200000;
     SAU->RLAR = 0x003fffe0 | SAU_RLAR_ENABLE_Msk;
     SAU->RNR = 1;
-    SAU->RBAR = 0x28200000;
-    SAU->RLAR = 0x283fffe0 | SAU_RLAR_ENABLE_Msk;
+    SAU->RBAR = 0x2820'0000;
+    SAU->RLAR = 0x283f'ffe0 | SAU_RLAR_ENABLE_Msk;
     // TODO: Non-Secure Callable region
 
     // Allow Non-Secure access to UART0
     SAU->RNR = 2;
-    SAU->RBAR = 0x40200000;
-    SAU->RLAR = 0x40200fe0 | SAU_RLAR_ENABLE_Msk;
+    SAU->RBAR = 0x4020'0000;
+    SAU->RLAR = 0x4020'0fe0 | SAU_RLAR_ENABLE_Msk;
+
+    // Configure SECRESPCFG to disable bus error on security violation
+    // *(volatile uint32_t *)0x5008'0010 &= ~(1 << 0);
 
     // Configure APB PPC EXP 1 NS (APBNSPPCEXP1) interface 5 to enable
     // Non-Secure access to UART0
-    *(volatile uint32_t *)0x50080084 |= 1 << 5;
+    *(volatile uint32_t *)0x5008'0084 |= 1 << 5;
 
     // Configure APB PPC EXP 1 SP (APBSPPPCEXP1) interface 5 to enable
     // unprivileged access to UART0
-    *(volatile uint32_t *)0x500800c4 |= 1 << 5;
+    *(volatile uint32_t *)0x5008'00c4 |= 1 << 5;
+
+    // Configure MPC to enable Non-Secure access to SSRAM1.
+    const auto SSRAM1_MPC_BASE = (volatile uint32_t *)0x5800'7000;
+    {
+        const auto CTRL = SSRAM1_MPC_BASE;
+        const auto BLK_MAX = SSRAM1_MPC_BASE + 0x10 / 4;
+        const auto BLK_IDX = SSRAM1_MPC_BASE + 0x18 / 4;
+        const auto BLK_LUT = SSRAM1_MPC_BASE + 0x1c / 4;
+
+        *CTRL |= 1 << 8; // Enable autoincrement
+
+        // Only the second half (`[BLX_MAX / 2, BLX_MAX - 1]`, which corresponds
+        // to `[0, 0x001f'0000]`) is allocated for Non-Secure access.
+        *BLK_IDX = *BLK_MAX / 2;
+        for (uint32_t i = *BLK_MAX / 2; i > 0; --i) {
+            *BLK_LUT = 0xffff'ffff;
+        }
+    }
+
+    // Configure MPC to enable Non-Secure access to SSRAM3.
+    const auto SSRAM3_MPC_BASE = (volatile uint32_t *)0x5800'9000;
+    {
+        const auto CTRL = SSRAM3_MPC_BASE;
+        const auto BLK_MAX = SSRAM3_MPC_BASE + 0x10 / 4;
+        const auto BLK_IDX = SSRAM3_MPC_BASE + 0x18 / 4;
+        const auto BLK_LUT = SSRAM3_MPC_BASE + 0x1c / 4;
+
+        *CTRL |= 1 << 8; // Enable autoincrement
+
+        *BLK_IDX = 0;
+        for (uint32_t i = *BLK_MAX; i > 0; --i) {
+            *BLK_LUT = 0xffff'ffff;
+        }
+    }
 
     TZ_SAU_Enable();
 
     // Set the Non-Secure main stack
-    __TZ_set_MSP_NS(*(uint32_t *)0x00200000);
+    __TZ_set_MSP_NS(*(uint32_t *)0x0020'0000);
 
-    auto nsResetHandler = (ns_funcptr_void)(*(uint32_t *)0x00200004 & ~1);
-    // Work-around a bug in QEMU that triggers UsageFault on VLSTM/VLLDM
-    // nsResetHandler();
-    asm volatile ("blxns %0"::"r"(nsResetHandler):);
+    auto nsResetHandler = (ns_funcptr_void)(*(uint32_t *)0x0020'0004 & ~1); // Secure alias of 0x20'0004
+    nsResetHandler();
 
     while (1)
         ;
