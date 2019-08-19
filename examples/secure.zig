3fa31a6e6d78d6e42e5bf6bfcd2e13670176355d
Tomoaki Kawada <tcpp.jp@gmail.com>
2019-08-19 23:52:14 +0900

    Add Secure panic handler

--- examples/secure.zig
+++ examples/secure.zig
@@ -1,2 +1,13 @@
 // The root source file for the Secure part of this project.
 export const _ = @import("secure/main.zig");
+
+const an505 = @import("drivers/an505.zig");
+const builtin = @import("builtin");
+
+/// The global panic handler. (The compiler looks for `pub fn panic` in the root
+/// source file. See `zig/std/special/panic.zig`.)
+pub fn panic(msg: []const u8, error_return_trace: ?*builtin.StackTrace) noreturn {
+    an505.uart0.print("panic: {}\r\n", msg);
+    @breakpoint();
+    unreachable;
+}
