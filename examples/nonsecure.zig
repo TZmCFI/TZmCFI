0b82b9c675589e86b94a82127673975caba1534f
Tomoaki Kawada <tcpp.jp@gmail.com>
2019-08-20 09:58:44 +0900

    Mark `panic` as cold

--- examples/nonsecure.zig
+++ examples/nonsecure.zig
@@ -8,6 +8,8 @@ const debugOutput = @import("nonsecure/debug.zig").debugOutput;
 /// The global panic handler. (The compiler looks for `pub fn panic` in the root
 /// source file. See `zig/std/special/panic.zig`.)
 pub fn panic(msg: []const u8, error_return_trace: ?*builtin.StackTrace) noreturn {
+    @setCold(true);
+
     debugOutput("panic: {}\r\n", msg);
     @breakpoint();
     unreachable;
--- examples/secure.zig
+++ examples/secure.zig
@@ -7,6 +7,8 @@ const builtin = @import("builtin");
 /// The global panic handler. (The compiler looks for `pub fn panic` in the root
 /// source file. See `zig/std/special/panic.zig`.)
 pub fn panic(msg: []const u8, error_return_trace: ?*builtin.StackTrace) noreturn {
+    @setCold(true);
+
     an505.uart0.print("panic: {}\r\n", msg);
     @breakpoint();
     unreachable;
