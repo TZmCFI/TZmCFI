e47c5e20cbe1ccc08304c9fcb388a2069ffbe062
Tomoaki Kawada <tcpp.jp@gmail.com>
2020-01-10 19:20:44 +0900

    examples: Get UART working on LPC55S69

--- examples/drivers/lpc55s69.zig
+++ examples/drivers/lpc55s69.zig
@@ -94,6 +94,226 @@ pub const flexcomm = [8]Flexcomm {
     Flexcomm { .base = 0x50098000 },
 };
 
+pub const Syscon = struct {
+    base: usize,
+
+    const Self = @This();
+
+    pub fn regAhbclkctrlset0(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x220);
+    }
+    pub fn regAhbclkctrlclr0(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x240);
+    }
+
+    pub const AHBCLKCTRL0_IOCON = bit(13);
+
+    pub fn regAhbclkctrlset1(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x224);
+    }
+    
+    pub fn ahbclkctrl1Fc(comptime i: u3) u32 {
+        return bit(11 + @as(u5, i));
+    }
+
+    pub fn regMainclkselb(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x284);
+    }
+
+    pub const MAINCLKSELB_SEL_PLL0: u32 = 1;
+
+    /// This register selects the clock source for the PLL0.
+    pub fn regPll0clksel(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x290);
+    }
+
+    pub const PLL0CLKSEL_CLKIN: u32 = 1;
+
+    pub fn regFcclksel(self: Self, i: u3) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x2b0 + @as(u32, i) * 4);
+    }
+    
+    pub const FCCLKSEL_PLL0DIV: u32 = 1;
+
+    pub fn regFlexfrgctrl(self: Self, i: u3) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x320 + @as(u32, i) * 4);
+    }
+
+    pub fn flexfrgctrlDiv(ratio: u8) u32 {
+        return @as(u32, ratio);
+    }
+
+    pub fn flexfrgctrlMult(ratio: u8) u32 {
+        return @as(u32, ratio) << 8;
+    }
+
+    /// This register determines the divider value for the PLL0 output, if used by the application.
+    pub fn regPll0clkdiv(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x3c4);
+    }
+
+    pub fn regAhbclkdiv(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x380);
+    }
+
+    /// Divide by `t + 1`
+    pub fn ahbclkdivDiv(t: u8) u32 {
+        return @as(u32, t);
+    }
+
+    pub fn regFmccr(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x400);
+    }
+
+    pub const FMCCR_FLASHTIM_MASK: u32 = fmccrFlashtim(0b11111);
+    pub fn fmccrFlashtim(t: u5) u32 {
+        return @as(u32, t) << 12;
+    }
+
+    /// The PLL0CTRL register provides most of the control over basic
+    /// selections of PLL0 modes and operating details.
+    pub fn regPll0ctrl(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x580);
+    }
+    
+    pub const Pll0ctrl = packed struct {
+        selr: u4 = 0,
+        seli: u6 = 0,
+        selp: u5 = 0,
+        bypasspll: bool = false,
+        bypasspostdiv2: bool = false,
+        limupoff: bool = false,
+        bwdirect: bool = false,
+        bypassprediv: bool = false,
+        bypasspostdiv: bool = false,
+        clken: bool = false,
+        frmen: bool = false,
+        frmclkstable: bool = false,
+        skewen: bool = false,
+        _pad: u7 = undefined,
+    };
+    comptime {
+        if (@sizeOf(Pll0ctrl) != 4) @compileError("@sizeOf(Pll0ctrl) is not 32-bit long");
+    }
+
+    pub fn regPll0stat(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x584);
+    }
+
+    pub const PLL0STAT_LOCK = bit(0);
+
+    /// The PLL0NDEC controls operation of the PLL pre-divider.
+    pub fn regPll0ndec(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x588);
+    }
+    
+    pub const PLL0NDEC_NREQ = bit(8);
+
+    pub fn setPll0NDividerRatio(self: Self, ratio: u8) void {
+        self.regPll0ndec().* = @as(u32, ratio) | PLL0NDEC_NREQ;
+    }
+
+    /// The PLL0PDEC controls operation of the PLL post-divider.
+    pub fn regPll0pdec(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x58c);
+    }
+
+    pub const PLL0PDEC_NREQ = bit(5);
+
+    pub fn setPll0PDividerRatio(self: Self, ratio: u5) void {
+        self.regPll0pdec().* = @as(u32, ratio) | PLL0PDEC_NREQ;
+    }
+
+    pub fn regPll0sscg0(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x590);
+    }
+
+    pub fn regPll0sscg1(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x594);
+    }
+
+    pub fn pll0sscg1MdivExt(x: u16) u32 {
+        return @as(u32, x) << 10;
+    }
+
+    pub const PLL0SSCG1_MREQ = bit(26);
+    pub const PLL0SSCG1_SEL_EXT = bit(28);
+
+    pub fn regCpuctrl(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x800);
+    }
+
+    pub fn regCpboot(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x804);
+    }
+
+    pub fn regClockCtrl(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0xa18);
+    }
+
+    pub const CLOCK_CTRL_CLKIN_ENA = bit(5);
+};
+
+pub const syscon = Syscon { .base = 0x50000000 };
+
+pub const Pmc = struct {
+    base: usize,
+
+    const Self = @This();
+
+    /// The power configuration clear register 0 controls the power to various
+    /// analog blocks.
+    pub fn regPdruncdfclr0(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0xc8);
+    }
+
+    pub const PDRUNCFG0_PDEN_XTAL32M = bit(8);
+    pub const PDRUNCFG0_PDEN_PLL0 = bit(9);
+    pub const PDRUNCFG0_PDEN_LDOXO32M = bit(20);
+};
+
+pub const pmc = Pmc { .base = 0x50020000 };
+
+/// Analog control
+pub const AnaCtrl = struct {
+    base: usize,
+
+    const Self = @This();
+
+    pub fn regXo32mCtrl(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x20);
+    }
+
+    pub const XO32M_CTRL_ENABLE_SYSTEM_CLK_OUT = bit(24);
+
+    pub fn regXo32mStatus(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x24);
+    }
+
+    pub const XO32M_STATUS_XO_READY = bit(0);
+};
+
+pub const ana_ctrl = AnaCtrl { .base = 0x50013000 };
+
+/// I/O Pin Configuration
+pub const Iocon = struct {
+    base: usize,
+
+    const Self = @This();
+
+    pub fn regP0(self: Self, i: u32) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + i * 4);
+    }
+    pub fn regP1(self: Self, i: u32) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x080 + i * 4);
+    }
+
+    pub fn pFunc(f: u32) u32 { return f; }
+    pub const P_DIGIMODE = bit(8);
+};
+
+pub const iocon = Iocon { .base = 0x50001000 };
+
 /// The number of hardware interrupt lines.
 pub const num_irqs = 60;
 
@@ -111,3 +331,7 @@ pub const irqs = struct {
         }
     }
 };
+
+fn bit(comptime n: u32) u32 {
+    return 1 << n;
+}
--- examples/ports/lpc55s69/secure.zig
+++ examples/ports/lpc55s69/secure.zig
@@ -11,15 +11,96 @@ extern var __nsc_end: usize;
 const Flexcomm = lpc55s69.Flexcomm;
 const usart = lpc55s69.flexcomm[0];
 
+const Syscon = lpc55s69.Syscon;
+const syscon = lpc55s69.syscon;
+
+const Pmc = lpc55s69.Pmc;
+const pmc = lpc55s69.pmc;
+
+const Iocon = lpc55s69.Iocon;
+const iocon = lpc55s69.iocon;
+
+const AnaCtrl = lpc55s69.AnaCtrl;
+const ana_ctrl = lpc55s69.ana_ctrl;
+
 /// Perform the board-specific initialization.
 pub fn init() void {
-    // Configure USART
+    // Configure Clock Tree
     // -----------------------------------------------------------------------
-    // TODO: configure clock
 
+    // Power up the crystal oscillator
+    pmc.regPdruncdfclr0().* = Pmc.PDRUNCFG0_PDEN_XTAL32M | Pmc.PDRUNCFG0_PDEN_PLL0;
+
+    // Enable CLKIN from the crystal oscillator
+    syscon.regClockCtrl().* |= Syscon.CLOCK_CTRL_CLKIN_ENA;
+
+    // Enable the 16MHz crystal oscilaltor
+    ana_ctrl.regXo32mCtrl().* |= AnaCtrl.XO32M_CTRL_ENABLE_SYSTEM_CLK_OUT;
+
+    // Wait for it to be stable
+    while ((ana_ctrl.regXo32mStatus().* & AnaCtrl.XO32M_STATUS_XO_READY) == 0) {}
+
+    // Power up PLL0
+    pmc.regPdruncdfclr0().* = Pmc.PDRUNCFG0_PDEN_PLL0;
+
+    // Select CLKIN as PLL0 input
+    syscon.regPll0clksel().* = Syscon.PLL0CLKSEL_CLKIN;
+
+    // Configure PLL0
+    syscon.setPll0NDividerRatio(4); // 16MHz / 4 → 4MHz
+    syscon.regPll0sscg1().* = 
+        Syscon.pll0sscg1MdivExt(75) | // 4MHz * 75 → 300MHz
+        Syscon.PLL0SSCG1_MREQ |
+        Syscon.PLL0SSCG1_SEL_EXT;
+    syscon.setPll0PDividerRatio(3); // 300MHz / 3 → 100MHz
+    syscon.regPll0ctrl().* = @bitCast(
+        u32,
+        Syscon.Pll0ctrl {
+            .selr = 0,  // selr = 0
+            .seli = 39, // seli = 2 * floor(M / 4) + 3 = 39
+            .selp = 19, // selp = floor(M / 4) + 1 = 19
+            .bypasspostdiv2 = true, // bypass post divide-by-2
+            .clken = true,
+        },
+    );
+
+    // Wait for PLL0 to lock
+    while ((syscon.regPll0stat().* & Syscon.PLL0STAT_LOCK) == 0) {}
+
+    // The required flash memory access time for system clock rates up to
+    // 100MHz is 9 system closk
+    syscon.regFmccr().* = syscon.regFmccr().*
+        & (~Syscon.FMCCR_FLASHTIM_MASK) | Syscon.fmccrFlashtim(8);
+    
+    // Select PLL0 output as main clock
+    syscon.regMainclkselb().* = Syscon.MAINCLKSELB_SEL_PLL0;
+
+    // AHBCLK = main_clk / 1
+    syscon.regAhbclkdiv().* = Syscon.ahbclkdivDiv(0);
+
+    // pll0_clk_div = 33.33...MHz
+    syscon.regPll0clkdiv().* = 3 - 1;
+
+    // Configure Flexcomm 0 clock to 33.33...MHz / (1 + 75 / 256) → 25.78...MHz (25600/993MHz)
+    syscon.regFlexfrgctrl(0).* = Syscon.flexfrgctrlDiv(0xff) | Syscon.flexfrgctrlMult(75);
+    syscon.regFcclksel(0).* = Syscon.FCCLKSEL_PLL0DIV;
+    
+    // Enable Flexcomm 0 clock
+    syscon.regAhbclkctrlset1().* = Syscon.ahbclkctrl1Fc(0);
+
+    // Configure USART (Flexcomm 0)
+    // -----------------------------------------------------------------------
+    
+    // Configure the I/O pins
+    syscon.regAhbclkctrlset0().* = Syscon.AHBCLKCTRL0_IOCON;
+    iocon.regP0(29).* = Iocon.pFunc(1) | Iocon.P_DIGIMODE; // RX: P0_29(1)
+    iocon.regP0(30).* = Iocon.pFunc(1); // TX: P0_30(1)
+    syscon.regAhbclkctrlclr0().* = Syscon.AHBCLKCTRL0_IOCON;
+
+    // Select USART
     usart.regPselid().* = Flexcomm.PSELID_PERSEL_USART;
 
-    usart.regUsartBrg().* = 14 - 1; // 25.8 MHz / (16 * 14) ~ 115207 bps
+    usart.regUsartBrg().* = 14 - 1; // 25.78 MHz / (16 * 14) ≈ 115091 bps
     usart.regFifoCfg().* = Flexcomm.FIFO_CFG_ENABLETX | Flexcomm.FIFO_CFG_ENABLERX;
     usart.regUsartCfg().* = Flexcomm.USART_CFG_ENABLE | Flexcomm.USART_CFG_DATALEN_8BIT;
 
