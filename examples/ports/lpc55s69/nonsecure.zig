0674e762c38dca6ef5434fdb810db31fb0cb08c4
Tomoaki Kawada <tcpp.jp@gmail.com>
2020-01-14 16:42:06 +0900

    lpc55s69: Zero the privileged bss section

--- examples/ports/lpc55s69/nonsecure.zig
+++ examples/ports/lpc55s69/nonsecure.zig
@@ -1,1 +1,10 @@
-pub fn init() void {}
+extern var __privileged_sram_start__: usize;
+extern var __privileged_sram_end__: usize;
+
+pub fn init() void {
+    // Clear the privileged data to 0 as the startup code is only set to
+    // clear the non-privileged bss.
+    const start = @ptrCast([*]u8, &__privileged_sram_start__);
+    const end = @ptrCast([*]u8, &__privileged_sram_end__);
+    @memset(start, 0, @ptrToInt(end) - @ptrToInt(start));
+}
