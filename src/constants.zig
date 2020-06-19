e34c0d11fd91fc0d77d076c511f4e7b2b5c067c8
Tomoaki Kawada <tcpp.jp@gmail.com>
2020-06-19 12:53:35 +0900

    perf: merge stage1 into stage0
    This reduces interrupt latency by 10 cycles (measured on LPC55S69).

--- src/constants.zig
+++ src/constants.zig
@@ -8,7 +8,7 @@ pub const VEC_TABLE = struct {
     pub const HDR_SIZE_MASK: usize = c.TC_VEC_TABLE_HDR_SIZE_MASK;
 
     pub const TRAMPOLINE_STRIDE: usize = c.TC_VEC_TABLE_TRAMPOLINE_STRIDE;
-    pub const TRAMPOLINE_STRIDE_SHIFT = 3;
+    pub const TRAMPOLINE_STRIDE_SHIFT = 4;
 };
 
 comptime {
